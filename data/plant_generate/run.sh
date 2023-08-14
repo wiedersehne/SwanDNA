78a0bcdd5fb68c5af05a9cea9592e801f4561d6b
LeiCheng-no <72392271+LeiCheng-no@users.noreply.github.com>
2023-08-14 11:42:41 +0200

    Update run.sh

--- data/plant_generate/run.sh
+++ data/plant_generate/run.sh
@@ -1,6 +1,6 @@
 python3 _cython_setup.py build_ext --inplace
 
-python data_label.py -plant ar -num_train 500
+python data_label.py -plant ar #-num_train 500
 #python data_label.py -plant bd
 #python data_label.py -plant mh
 #python data_label.py -plant sb
@@ -8,7 +8,7 @@ python data_label.py -plant ar -num_train 500
 #python data_label.py -plant zm
 #python data_label.py -plant zs
 
-python data_pre.py -length 1000 -num_train 500
+python data_pre.py -length 1000 #-num_train 500
 #python data_pre.py -length 10000
 #python data_pre.py -length 50000
 #python data_pre.py -length 100000
